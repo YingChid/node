0b606700793f4aa8684e3ee07901837d5d92c2d1
YingChid <monm.ygchid04@gmail.com>
2019-10-31 17:44:35 +0700

    Added path all product

--- routes/products.js
+++ routes/products.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/all', function(req, res, next) {
+  res.send('get all product);
+});
+
 module.exports = router;
